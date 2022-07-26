b9ab228b108fe8584b8e528f0c96cd500ead04a1
Sergey Royz <zjor.se@gmail.com>
2022-07-26 23:41:20 +0200

    toggl client

--- mqtt-notifier/toggl-track.js
+++ mqtt-notifier/toggl-track.js
@@ -1,6 +1,9 @@
 import got from 'got'
 import config from 'config'
+
 const toggl = config.get('toggl')
+
+const log = console.log
 
 const baseUrl = 'https://api.track.toggl.com/api/v9'
 const basicAuthHash = btoa(`${toggl.token}:api_token`)
@@ -18,64 +21,64 @@ async function me() {
 async function getCurrentTimeEntry() {
     return got.get(`${baseUrl}/me/time_entries/current`, {...options})
 }
-/*
-{"billable":"boolean",
-"created_with":"string",
-"description":"string",
-"duration":"integer",
-"duronly":"boolean",
-"pid":"integer",
-"postedFields":["string"],
-"project_id":"integer",
-"start":"string",
-"start_date":"string",
-"stop":"string",
-"tag_action":"string",
-"tag_ids":["integer"],
-"tags":["string"],
-"task_id":"integer",
-"tid":"integer",
-"uid":"integer",
-"user_id":"integer",
-"wid":"integer",
-"workspace_id":"integer"},
-*/
 
-/*
-'{"created_with":"API example code","pid":null,"tid":null,"description":"Hello Toggl","tags":[],"billable":false,"duration":-1654686174,"wid":1,"at":"1984-06-08T11:02:53.836Z","start":"1984-06-08T11:02:53.000Z","stop":null}' \
-*/
-
-async function startTimeEntry(workspaceId, projectId, description) {
-    // const now = new Date().toISOString()
-    const now = '2022-07-26T13:05:18+00:00'
+async function startTimeTracking(workspaceId, projectId, description) {
+    const now = new Date().toISOString()
     const json = {
-        wid: workspaceId,
-        workspace_id: workspaceId,
-        pid: projectId,
-        project_id: projectId,
-        created_with: 'IoT-toggle',
-        description: description,
-        billable: false,
-        duration: -1654686174,
-        duronly: false,
-        at: now,
-        start: now,
-        stop: null
+        "created_with": "nodeJS",
+        "pid": projectId,
+        "tid": null,
+        description,
+        "tags": [],
+        "billable": false,
+        "duration": -1658844435,
+        "wid": workspaceId,
+        "start": now,
+        "stop": null,
+        "at": now
     }
     return got.post(`${baseUrl}/workspaces/${workspaceId}/time_entries`, {...options, json})
+}
+
+async function stopTimeTracking() {
+    const current = JSON.parse((await getCurrentTimeEntry()).body)
+    if (!current) {
+        console.log("No running tasks")
+        return
+    }
+    console.log(`Stopping ${JSON.stringify(current)}`)
+
+
+    const { id, workspace_id: workspaceId, start } = current
+    const now = new Date()
+    const startDate = new Date(start)
+
+    log(`Duration: ${now - startDate}`)
+
+    const json = {
+        duration: Math.ceil((now - startDate) / 1000),
+        workspace_id: workspaceId
+    }
+
+    log(`Payload: ${JSON.stringify(json)}`)
+
+    return got.put(`${baseUrl}/workspaces/${workspaceId}/time_entries/${id}`, {...options, json})
 }
 
 async function main() {
     try {
         const workspaceId = 224075;
         const projectId = 163308321;
-        const createTaskResponse = await startTimeEntry(workspaceId, projectId, "Hello from NodeJS")
+        const createTaskResponse = await startTimeTracking(workspaceId, projectId, "Hello from NodeJS")
         console.log(createTaskResponse.body)
         const response = await getCurrentTimeEntry()
         console.log(response.statusCode)
         console.log(response.body)
+
+        const stopResponse = await stopTimeTracking()
+        log(`HTTP code: ${stopResponse.statusCode}`)
     } catch (e) {
-        console.error(`Error: ${e}`)
+        console.error(`Error: ${e.response.body}`)
     }
 }
 
